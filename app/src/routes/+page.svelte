2444503917c814798fd8a77acdfeba05e1a9a6f6
herjuus <herjus420@gmail.com>
2024-06-12 18:09:00 +0200

    fix trust. Added waiting for websocket to get enabled

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,148 +1,150 @@
-<script lang="ts">
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
+  <script lang="ts">
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
 
-  type SignalMessage = {
-  kind: 'offer' | 'answer' | 'candidate';
-  data: string;
-  };
+    type SignalMessage = {
+    kind: 'offer' | 'answer' | 'candidate';
+    data: string;
+    };
 
-  let localVideoRef: HTMLVideoElement;
-  let remoteVideoRef: HTMLVideoElement;
-  let localStream: MediaStream;
-  let remoteStream: MediaStream;
-  let peerConnection: RTCPeerConnection;
-  let ws: WebSocket;
-  let roomId = writable<string>('');
-  let inCall = writable<Boolean>(false);
+    let localVideoRef: HTMLVideoElement;
+    let remoteVideoRef: HTMLVideoElement;
+    let localStream: MediaStream;
+    let remoteStream: MediaStream;
+    let peerConnection: RTCPeerConnection;
+    let ws: WebSocket;
+    let roomId = writable<string>('');
+    let inCall = writable<Boolean>(false);
 
-  const servers = {
-    iceServers: [
-      {
-        urls: 'stun:stun.l.google.com:19302'
-      }
-    ]
-  };
+    const servers = {
+      iceServers: [
+        {
+          urls: 'stun:stun.l.google.com:19302'
+        }
+      ]
+    };
 
-  async function getRandomRoom() {
-  const response = await fetch(`https://api.ogiggle.herjus.no/join_random_room`);
-  const data = await response.json();
-  roomId.set(data.room_id);
-  return data.room_id;
-  }
-
-  onMount(async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    });
-    localVideoRef.srcObject = localStream;
-  })
-
-  async function startCall() {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.close();
+    async function getRandomRoom() {
+    const response = await fetch(`https://api.ogiggle.herjus.no/join_random_room`);
+    const data = await response.json();
+    roomId.set(data.room_id);
+    return data.room_id;
     }
 
-    const roomId = await getRandomRoom();
-    ws = new WebSocket(`wss://api.ogiggle.herjus.no/ws/${roomId}`);
-
-    ws.onmessage = handleSignalingMessage;
-
-    try {
+    onMount(async () => {
       localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
       });
       localVideoRef.srcObject = localStream;
+    })
 
-      peerConnection = new RTCPeerConnection(servers);
-      localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+    async function startCall() {
+      if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.close();
+      }
 
-      peerConnection.ontrack = (event: RTCTrackEvent) => {
-        remoteStream = event.streams[0];
-        remoteVideoRef.srcObject = remoteStream;
-      };
+      const roomId = await getRandomRoom();
+      ws = new WebSocket(`wss://api.ogiggle.herjus.no/ws/${roomId}`);
 
-      peerConnection.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
-        if (event.candidate) {
-          sendMessage({ kind: 'candidate', data: JSON.stringify(event.candidate) });
+      ws.onmessage = handleSignalingMessage;
+
+      ws.onopen = async () => {
+        try {
+          localStream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+          });
+          localVideoRef.srcObject = localStream;
+  
+          peerConnection = new RTCPeerConnection(servers);
+          localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+  
+          peerConnection.ontrack = (event: RTCTrackEvent) => {
+            remoteStream = event.streams[0];
+            remoteVideoRef.srcObject = remoteStream;
+          };
+  
+          peerConnection.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
+            if (event.candidate) {
+              sendMessage({ kind: 'candidate', data: JSON.stringify(event.candidate) });
+            }
+          };
+  
+          const offer = await peerConnection.createOffer();
+          await peerConnection.setLocalDescription(offer);
+          sendMessage({ kind: 'offer', data: JSON.stringify(offer) });
+  
+          inCall.set(true);
+        } catch (error) {
+          throw new Error(JSON.stringify(error));
         }
-      };
-
-      const offer = await peerConnection.createOffer();
-      await peerConnection.setLocalDescription(offer);
-      sendMessage({ kind: 'offer', data: JSON.stringify(offer) });
-
-      inCall.set(true);
-    } catch (error) {
-      throw new Error(JSON.stringify(error));
-    }
-  }
-
-  async function handleSignalingMessage(event: MessageEvent) {
-    const message: SignalMessage = await JSON.parse(event.data);
-
-    if (message.kind === 'offer') {
-      await peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data)));
-      const answer = await peerConnection.createAnswer();
-      await peerConnection.setLocalDescription(answer);
-      await sendMessage({ kind: 'answer', data: JSON.stringify(answer) });
-    } else if (message.kind === 'answer') {
-      await peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data)));
-    } else if (message.kind === 'candidate') {
-      await peerConnection.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data)));
-    }
-  }
-
-  function sendMessage(message: SignalMessage) {
-    if (ws) {
-      ws.send(JSON.stringify(message));
-    }
-  }
-
-  function disconnectCall() {
-    if (peerConnection) {
-      peerConnection.close();
-    }
-    if (remoteStream) {
-      remoteStream.getTracks().forEach(track => track.stop());
-    }
-    if (ws) {
-      ws.close();
+      }
     }
 
-    inCall.set(false);
-  }
+    async function handleSignalingMessage(event: MessageEvent) {
+      const message: SignalMessage = await JSON.parse(event.data);
 
-  function skipCall() {
-    disconnectCall();
-    startCall();
-  }
-</script>
+      if (message.kind === 'offer') {
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data)));
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        await sendMessage({ kind: 'answer', data: JSON.stringify(answer) });
+      } else if (message.kind === 'answer') {
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data)));
+      } else if (message.kind === 'candidate') {
+        await peerConnection.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data)));
+      }
+    }
 
-<main class="max-h-screen">
-    <header class="h-16 p-2 flex items-center">
-        <img src="logo.png" alt="" class="h-full">
-    </header>
-    <div class="grid grid-cols-2 px-2 gap-2">
-        <div class="aspect-[4/3] overflow-hidden bg-black/10 rounded">
-            <video class="-scale-x-100 w-full " bind:this={remoteVideoRef} autoplay playsinline></video>
-        </div>
-        <div class="aspect-[4/3] flex items-center justify-center overflow-hidden bg-black/10 rounded">
-            <video class="-scale-x-100 w-full " bind:this={localVideoRef} autoplay playsinline muted></video>
-        </div>
+    function sendMessage(message: SignalMessage) {
+      if (ws) {
+        ws.send(JSON.stringify(message));
+      }
+    }
 
-        <div class="w-full h-full space-y-2">
-          {#if !$inCall}
-            <button on:click={startCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Start</button>
-          {:else}
-            <button on:click={skipCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Skip</button>
-          {/if}
-          <button disabled={!$inCall} on:click={disconnectCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Stop</button>
-        </div>
-        <div>
+    function disconnectCall() {
+      if (peerConnection) {
+        peerConnection.close();
+      }
+      if (remoteStream) {
+        remoteStream.getTracks().forEach(track => track.stop());
+      }
+      if (ws) {
+        ws.close();
+      }
 
-        </div>
-    </div>
-</main>
+      inCall.set(false);
+    }
+
+    function skipCall() {
+      disconnectCall();
+      startCall();
+    }
+  </script>
+
+  <main class="max-h-screen">
+      <header class="h-16 p-2 flex items-center">
+          <img src="logo.png" alt="" class="h-full">
+      </header>
+      <div class="grid grid-cols-2 px-2 gap-2">
+          <div class="aspect-[4/3] overflow-hidden bg-black/10 rounded">
+              <video class="-scale-x-100 w-full " bind:this={remoteVideoRef} autoplay playsinline></video>
+          </div>
+          <div class="aspect-[4/3] flex items-center justify-center overflow-hidden bg-black/10 rounded">
+              <video class="-scale-x-100 w-full " bind:this={localVideoRef} autoplay playsinline muted></video>
+          </div>
+
+          <div class="w-full h-full space-y-2">
+            {#if !$inCall}
+              <button on:click={startCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Start</button>
+            {:else}
+              <button on:click={skipCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Skip</button>
+            {/if}
+            <button disabled={!$inCall} on:click={disconnectCall} class="w-full bg-ogiggle-light disabled:bg-ogiggle-dark font-bold text-gray-50 text-3xl rounded p-2 border-b-8 border-ogiggle-dark disabled:border-ogiggle-light enabled:hover:scale-[98%] duration-200">Stop</button>
+          </div>
+          <div>
+
+          </div>
+      </div>
+  </main>
